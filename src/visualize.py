597887e562fbfa6426d10731bfcaa91033c29f52
edcohen08 <31940792+edcohen08@users.noreply.github.com>
2019-05-06 20:19:24 -0400

    thanks Peter<3

--- src/visualize.py
+++ src/visualize.py
@@ -1,29 +1,30 @@
 import csv
-import numpy as np
-import matplotlib.pyplot as plt
+f = open('output_final_no_appeal.csv')
+csv_f = csv.reader(f)
+false_p = 0
+false_n = 0
+true_p = 0
 
-def results_for_csv(file):     # resuses code from recall_precision to calculate values
-	f = open(file)
-	csv_f = csv.reader(f)
-	false_p = 0
-	false_n = 0
-	true_p = 0	
+for row in csv_f:
+	if row[0] == 'hit_id':
+		continue
+	if row[1] == 'UNFAIR' and row[2] == 'UNFAIR':
+		true_p = true_p + 1
+	elif row[1] == 'FAIR' and row[2] == 'UNFAIR':
+		false_p = false_p + 1
+	elif row[1] == 'UNFAIR' and row[2] == 'FAIR':
+		false_n = false_n + 1
 
+recall = true_p/(true_p + false_n)
+precision = true_p/(true_p + false_p)
+f = 2 * ((precision * recall)/(precision + recall))
 
-	for row in csv_f:
-		if row[0] == 'hit_id':
-			continue
-		if row[1] == 'FAIR' and row[2] == 'FAIR':
-			true_p = true_p + 1
-		elif row[1] == 'FAIR' and row[2] == 'UNFAIR':
-			false_p = false_p + 1
-		elif row[1] == 'UNFAIR' and row[2] == 'FAIR':
-			false_n = false_n + 1
-
-	recall = true_p/(true_p + false_n)
-	precision = true_p/(true_p + false_p)
-	f = 2 * ((precision * recall)/(precision + recall))
-	return recall,precision,f
+print('F_1 SCORE:')
+print(f)
+print('RECALL SCORE:')
+print(recall)
+print('PRECISION SCORE:')
+print(precision)
 
 
 def visualize(no_appeal,w_appeal):		# Code to make a multi-series bar graph found at https://pythonspot.com/matplotlib-bar-chart/
